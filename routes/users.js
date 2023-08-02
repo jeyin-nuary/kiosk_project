01cbb3046632e668ad767064d829ceb0b2bb5166
jeyin-nuary <122019196+jeyin-nuary@users.noreply.github.com>
2023-08-02 20:15:40 +0900

    feat:로그인 api, auth-middleware 구현/ 썬더클라이언트 적용 전

--- routes/users.js
+++ routes/users.js
@@ -36,8 +36,56 @@ router.post('/signup', async (req, res) => {
         return res.status(412).json({errorMessage: '비밀번호의 값이 일치하지 않습니다.'});
       }
 
+
+      // 회원가입 성공! 뿌슝 빠슝-
+      const newUser = new Users({ nickname, password});
+      await Users.save();
+
+      return res.status(201).send({message: '회원가입을 축하합니다!'});
+
     } catch (error) {
         return res.status(400).json({errorMessage: '요청한 데이터 형식이 올바르지 않습니다.'});
     }
 })
 
+
+// 로그인 api
+router.post('/login', async (req, res) => {
+  try {
+    const {nickname, password} = req.body;
+
+  
+    // 닉네임 또는 비밀번호가 데이터베이스에 등록된 것과 맞는 지 확인, 불일치 시 에러 메시지
+    const existNickname = await Users.findOne({where: {nickname: nickname}});
+    const existPassword = await Users.findOne({where: {password: password}});
+
+    if (existNickname !== nickname || existPassword !== password) {
+      return res.status(412).json({message: '닉네임 또는 비밀번호를 확인해주세요.'});
+    }
+
+    // 로그인 성공 시, 유저 정보 jwt 활용 쿠키 전달
+    // 이 부분 코드 공부 더 필요. 다시 보기
+    const token = jwt.sign({ userId: Users.userId}, "customized-secret-key");
+
+    // jwt를 쿠키로 할당
+    res.cookie("Authorization", `Bearer ${token}`);
+    // jwt를 body로 할당
+    res.status(200).json({ token });
+
+  } catch (error) {
+    return res.status(400).json({errorMessage: ' 로그인에 실패했습니다.'});
+  }
+});
+
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+module.exports = router;
